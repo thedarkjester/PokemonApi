67f157966215b8b46bfb7b7c25de3a2644b4ea03
Grant Southey <thedarkjester@gmail.com>
2021-06-29 09:00:12 +0100

    some comments on future testing

---
+++ code/tests/Pokemon.Tests.Service/OtherTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon.Tests.Service
+{
+
+    // I would follow the same approach for service testing making sure most behaviours are covered and expected responses are returned
+    class OtherTests
+    {
+    }
+}
---
+++ code/tests/Pokemon.Tests.Unit/Othertests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon.Tests.Service
+{
+
+    // I would cover most of the processor and orchestator logic as well to make sure it will behave as expected
+    class OtherTests
+    {
+    }
+}
